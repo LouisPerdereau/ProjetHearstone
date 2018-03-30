b147016b11a2d8f109a0f5d05f4efa852de357ac
LouisPerdereau <louisleo.perdereau@gmail.com>
2018-03-30 13:53:00 +0200

    V0.1

--- app/src/main/java/com/example/e164726a/projethearthstone/MainActivity.java
+++ app/src/main/java/com/example/e164726a/projethearthstone/MainActivity.java
@@ -1,48 +1,36 @@
 package com.example.e164726a.projethearthstone;
 
-import android.preference.PreferenceActivity;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
+import android.widget.Spinner;
+
+
+
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.async.http.AsyncHttpClient;
-import com.koushikdutta.async.parser.AsyncParser;
-import com.koushikdutta.ion.Ion;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.koushikdutta.ion.Ion;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinClass;
+    private Spinner spinType;
+    private Spinner spinFaction;
+    private Spinner spinRace;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            HttpResponse<JsonNode> response = Unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards")
-                    .header("X-Mashape-Key", "zlietzvXopmshQaw5kLpHhPRJ9ggp1s1rfPjsn3PhcjtWFHUf7")
-                    .asJson();
-        } catch (UnirestException e) {
-            e.printStackTrace();
-        }
-
-   /*     Ion.with(MainActivity.this)
+        Ion.with(MainActivity.this)
                 .load("https://omgvamp-hearthstone-v1.p.mashape.com/cards")
-                .addHeader("X-Mashape-Key","zlietzvXopmshQaw5kLpHhPRJ9ggp1s1rfPjsn3PhcjtWFHUf7")
+                .addHeader("X-Mashape-Key","ffCXysEgCMmshgWPOSVnNtrl3kllp1YejDMjsnP8cQy6RhhHBl")
+                .addHeader("X-Mashape-Host", "omgvamp-hearthstone-v1.p.mashape.com")
                 .asJsonObject()
 
                 .setCallback(new FutureCallback<JsonObject>() {
@@ -53,6 +41,6 @@ public class MainActivity extends AppCompatActivity {
 
                     }
 
-                });*/
+                });
     }
 }
